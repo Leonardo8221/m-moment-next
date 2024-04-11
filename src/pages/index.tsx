578f03fd122c8161a9f7a0c83343736b29d9efc4
Leonardo8221 <r.leonardo0802@gmail.com>
2024-04-11 21:34:07 +0300

    Use page router

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import Layout from "@/Components/layout";
+import Home from "@/Components/PageFeeds/Home";
+import "../app/globals.css";
+
+export default function HomePage() {
+  return (
+    <Layout>
+      <Home />
+    </Layout>
+  );
+}
