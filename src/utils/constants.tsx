97bbeeed16c680802d51d97a7138136e619bc4a0
Leonardo8221 <r.leonardo0802@gmail.com>
2024-04-13 22:38:54 +0300

    Complete FAQs page

--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -143,23 +143,39 @@ export const faqsData = [
       "Yes, there is currently a limit of 10 users that can participate in a chat session.Yes, there is currently a limit of 10 users that can participate in a chat session.",
   },
   {
-    question: "How do I use MessageMoment?",
+    question: "Can I change my handle after it has been set?",
     answer:
-      "To use MessageMoment, simply open the chat link in your default or favorite browser. No account registration is necessary. The user will enter the chat session and gain visibility of the conversation only once they have entered their handle and token password (if applicable).",
+      "No, once you set your handle, you cannot change it. MessageMoment is typically used for chat conversations between known parties and so expectations should be set amongst the party of users when sharing the chat link with them, as to what to expect. Further, you can choose to validate who you are speaking with through your own screening questions once you arrive at the chat, if you feel compelled to.",
   },
   {
-    question: "How do I use MessageMoment?",
+    question: "How long will my chat data be available for?",
     answer:
-      "To use MessageMoment, simply open the chat link in your default or favorite browser. No account registration is necessary. The user will enter the chat session and gain visibility of the conversation only once they have entered their handle and token password (if applicable).",
+      'Chat data will be available for as long as the chat link remains active however this is subject to the Message Expiry time which will determine the length of time in which each user’s entered message will appear until it is no longer visible to anyone in the chat session. The Message Expiry time is set once by one user and applies to all users. Once it is set, it will appear inactive (grayed out) for all users indicating it has been set and what it has been set to. It cannot be changed for the remainder of chat session. It is the responsibility of the users to capture or save any information they want to retain, as the platform is provided on an "as is" and "as available" basis. Whilst not essential, for additional peace of mind, consider launching your browser in its private mode. This way, your visit to our site will not be recorded in your browser history.',
   },
   {
-    question: "How do I use MessageMoment?",
+    question:
+      "What if I or someone else accidently sets the wrong Message Expiry time?",
     answer:
-      "To use MessageMoment, simply open the chat link in your default or favorite browser. No account registration is necessary. The user will enter the chat session and gain visibility of the conversation only once they have entered their handle and token password (if applicable).",
+      "Once the Message Expiry time is set, it cannot be changed. In order to reset it, a new chat session needs to be established and the old chat link/session will be disregarded. By default, the Message Expiry time is set to 10 seconds however it can range from 3 second up to 5 minutes. Anyone that arrives at the chat session can choose to set it if.",
   },
   {
-    question: "How do I use MessageMoment?",
+    question: "Is my data safe with MessageMoment?",
     answer:
-      "To use MessageMoment, simply open the chat link in your default or favorite browser. No account registration is necessary. The user will enter the chat session and gain visibility of the conversation only once they have entered their handle and token password (if applicable).",
+      "MessageMoment takes the privacy of its users seriously and has implemented appropriate security measures to protect user data. However, users are responsible for ensuring that they do not share any sensitive or personal information through the chat service.",
+  },
+  {
+    question: "Can I use MessageMoment for commercial purposes?",
+    answer:
+      "MessageMoment is intended for personal use only and typically by known parties. Any commercial use is strictly prohibited.",
+  },
+  {
+    question: "Can my access to MessageMoment be terminated?",
+    answer:
+      "MessageMoment reserves the right to terminate or restrict your use of the platform at any time, with or without notice, for any or no reason, and without liability to you.",
+  },
+  {
+    question:
+      "Who can I contact if I have any questions, concerns or feedback about MessageMoment?",
+    answer: "You can Contact Us to get in touch.",
   },
 ];
