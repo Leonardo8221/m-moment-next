d4fedb1908e71b3092e22f5124c157017cf80f16
Leonardo8221 <r.leonardo0802@gmail.com>
2024-04-20 01:12:53 +0300

    404 page fix

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -8,7 +8,7 @@ export default function ChatFull() {
   return (
     <>
       <Head>
-        <title>404 - not found</title>
+        <title>404 - Not Found</title>
       </Head>
       <div className="flex flex-col h-[100vh]">
         <div className="flex justify-center border-t-[5px] border-t-[--blue] p-[17px_60px] border-b border-b-black/5">
@@ -19,23 +19,23 @@ export default function ChatFull() {
         <div className="py-[50px] relative flex flex-1 flex-col justify-between bg-gradient-to-b from-[#494af8]/10 to-transparent">
           <div className="absolute w-[470px] h-[470px] top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] pt-[88px] px-[87px] pb-[41px] bg-[#494af8]/50 filter blur-[100px]"></div>
           <div className="flex-1 flex items-center justify-center text-center">
-            <div className="flex flex-col bg-white pt-[134px] justify-between pb-[66px] w-[720px] h-[400px] relative border border-black/10 rounded-[10px]">
+            <div className="bg-white pt-[70px] pb-[40px] w-[720px] h-[430px] relative border border-black/10 rounded-[10px]">
               <Image
                 width={128}
                 height={128}
-                src={"/img/chat.svg"}
+                src={"/img/404.svg"}
                 alt=""
                 className="absolute left-[50%] translate-x-[-50%] top-[-64px]"
               />
-              <div className="flex flex-col justify-center items-center">
-                <h3 className="font-[DMSans] text-[26px] font-bold text-[--blue] text-center mb-[30px]">
-                  The chat session is full! There are
-                  <br /> currently 10/10 users joined
-                </h3>
-                <p className="font-[JetBrainsMono] font-normal text-[15px] text-[--dark] text-center mb-[33px]">
-                  Unfortunately you are unable to enter the chat at this time.
-                </p>
-              </div>
+              <h1 className="font-[DMSans] text-[150px] text-[--blue] text-center mb-0">
+                404
+              </h1>
+              <h3 className="font-[DMSans] text-[26px] font-bold text-[--blue] text-center mb-[30px]">
+                The page you’re looking for doesn&rsquo;t exist
+              </h3>
+              <p className="font-[JetBrainsMono] text-[15px] text-[--dark] text-center mb-[33px]">
+                Don’t panic, just click the big button below and return home.
+              </p>
               <Link
                 href={"/"}
                 className="mx-auto w-[236px] h-[46px] inline-flex items-center justify-center text-white rounded-[6px] bg-[--blue] font-[JetBrainsMono] !transition-all !duration-500 !ease-in-out hover:!bg-[--hoverblue]"
