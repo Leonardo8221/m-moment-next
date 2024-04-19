37ebaefe9846fb7a5937c8e85a5805d86e1c0f7f
Leonardo8221 <r.leonardo0802@gmail.com>
2024-04-19 23:50:42 +0300

    fix cookie issue

--- src/Components/layout/index.tsx
+++ src/Components/layout/index.tsx
@@ -3,6 +3,7 @@ import { navItems } from "@/utils/constants";
 import Header from "../Header";
 import Footer from "../Footer";
 import Head from "next/head";
+import Cookies from "../Cookies";
 
 interface Props {
   children: ReactNode;
@@ -18,6 +19,9 @@ function Layout({ children }: Props) {
         <Header items={navItems} />
         <div className="flex-1 w-full px-0">{children}</div>
         <Footer items={navItems} />
+        <div className="fixed bottom-[30px] right-[30px] z-[99]">
+          <Cookies />
+        </div>
       </div>
     </>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,11 @@
 import Layout from "@/Components/layout";
 import Home from "@/Components/PageFeeds/Home";
 import "../app/globals.css";
-import Cookies from "@/Components/Cookies";
 
 export default function HomePage() {
-  const zindex = {
-   zIndex:99
-  };
   return (
     <Layout>
       <Home />
-      <div className="fixed bottom-[30px] right-[30px]" style={zindex}>
-        <Cookies />
-      </div>
     </Layout>
   );
 }
